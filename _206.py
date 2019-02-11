c337ab2109a51e87017e7db72ac57e04a670d347
Gzp_ <741424975@qq.com>
2019-02-11 22:31:52 +0800

    [ADD] add 206

---
+++ _206.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__author__ = 'gzp'
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p, rev = head, None
+
+        while p:
+            rev, rev.next, p = p, rev, p.next
+
+        return rev
+
+
+
+
+
+
