68ced432f54d5037948e761e3994eddf36311a93
Gzp_ <741424975@qq.com>
2019-02-18 22:14:04 +0800

    [ADD] add 645

---
+++ 645.set-mismatch.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=645 lang=python3
+#
+# [645] 错误的集合
+#
+# https://leetcode-cn.com/problems/set-mismatch/description/
+#
+# algorithms
+# Easy (33.59%)
+# Total Accepted:    2.4K
+# Total Submissions: 7.2K
+# Testcase Example:  '[1,2,2,4]'
+#
+# 集合 S 包含从1到 n
+# 的整数。不幸的是，因为数据错误，导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重复。
+# 
+# 给定一个数组 nums 代表了集合 S 发生错误后的结果。你的任务是首先寻找到重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+# 
+# 示例 1:
+# 
+# 
+# 输入: nums = [1,2,2,4]
+# 输出: [2,3]
+# 
+# 
+# 注意:
+# 
+# 
+# 给定数组的长度范围是 [2, 10000]。
+# 给定的数组是无序的。
+# 
+# 
+#
+class Solution:
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        nums_set = set(nums)
+        return [sum(nums) - sum(nums_set), sum([x for x in range(1, len(nums) + 1)]) - sum(nums_set)]
