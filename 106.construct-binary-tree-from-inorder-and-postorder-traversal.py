f14076ef00f22935770f0e0f4df180afa2b31803
Gzp_ <741424975@qq.com>
2019-02-19 22:39:35 +0800

    [ADD] add 106

---
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,65 @@
+#
+# @lc app=leetcode.cn id=106 lang=python3
+#
+# [106] 从中序与后序遍历序列构造二叉树
+#
+# https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+#
+# algorithms
+# Medium (57.47%)
+# Total Accepted:    4.9K
+# Total Submissions: 8.6K
+# Testcase Example:  '[9,3,15,20,7]\n[9,15,7,20,3]'
+#
+# 根据一棵树的中序遍历与后序遍历构造二叉树。
+#
+# 注意:
+# 你可以假设树中没有重复的元素。
+#
+# 例如，给出
+#
+# 中序遍历 inorder = [9,3,15,20,7]
+# 后序遍历 postorder = [9,15,7,20,3]
+#
+# 返回如下的二叉树：
+#
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+#
+#
+#
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if not postorder:
+            return None
+        
+        root = TreeNode(postorder[-1])
+        mid =  inorder.index(root.val)
+
+        inorder_left = inorder[:mid]
+        inorder_right = inorder[mid + 1:]
+        
+        postorder_left = postorder[:len(inorder_left)]
+        postorder_right = postorder[len(inorder_left):-1]
+
+        root.left = self.buildTree(inorder_left, postorder_left)
+        root.right = self.buildTree(inorder_right, postorder_right)
+        
+        return root
+
