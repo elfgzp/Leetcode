5acb6fe2f6ce54f7241795e6ab11ac87fba2accd
Gzp_ <741424975@qq.com>
2019-02-19 13:32:09 +0800

    [ADD] add 46

---
+++ 46.permutations.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=46 lang=python3
+#
+# [46] 全排列
+#
+# https://leetcode-cn.com/problems/permutations/description/
+#
+# algorithms
+# Medium (65.30%)
+# Total Accepted:    13.9K
+# Total Submissions: 21.3K
+# Testcase Example:  '[1,2,3]'
+#
+# 给定一个没有重复数字的序列，返回其所有可能的全排列。
+#
+# 示例:
+#
+# 输入: [1,2,3]
+# 输出:
+# [
+# ⁠ [1,2,3],
+# ⁠ [1,3,2],
+# ⁠ [2,1,3],
+# ⁠ [2,3,1],
+# ⁠ [3,1,2],
+# ⁠ [3,2,1]
+# ]
+#
+#
+
+
+class Solution:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        from itertools import permutations
+        return [list(t) for t in permutations(nums, len(nums))]
+
