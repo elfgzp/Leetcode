9da7456ca39c2340de3f9f3f5246d4bd12bafc16
Gzp_ <741424975@qq.com>
2019-02-22 11:12:52 +0800

    [ADD] add 217

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode.cn id=217 lang=python3
+#
+# [217] 存在重复元素
+#
+# https://leetcode-cn.com/problems/contains-duplicate/description/
+#
+# algorithms
+# Easy (45.80%)
+# Total Accepted:    40K
+# Total Submissions: 87.2K
+# Testcase Example:  '[1,2,3,1]'
+#
+# 给定一个整数数组，判断是否存在重复元素。
+#
+# 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+#
+# 示例 1:
+#
+# 输入: [1,2,3,1]
+# 输出: true
+#
+# 示例 2:
+#
+# 输入: [1,2,3,4]
+# 输出: false
+#
+# 示例 3:
+#
+# 输入: [1,1,1,3,3,4,3,2,4,2]
+# 输出: true
+#
+#
+
+
+class Solution1:
+    def containsDuplicate(self, nums: 'List[int]') -> 'bool':
+        return len(set(nums)) != len(nums)
+
+
+class Solution:
+    def containsDuplicate(self, nums: 'List[int]') -> 'bool':
+        if len(nums) <= 0:
+            return False
+
+        nums = sorted(nums, key=lambda x: x)
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+
+        return False
