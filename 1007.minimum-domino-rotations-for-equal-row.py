98027c4f46b21a4df56394a47ae331fa8a98fc0a
Gzp_ <741424975@qq.com>
2019-03-10 12:43:37 +0800

    [ADD] add 1007

--- 1007.minimum-domino-rotations-for-equal-row.py
+++ 1007.minimum-domino-rotations-for-equal-row.py
@@ -1,16 +1,15 @@
 class Solution:
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
         length = len(A)
-        
+
         if length == 1:
             return 0
-        
+
         from itertools import permutations
         cases = (A[0], B[0])
         LS = (A, B)
         res = []
-        
-        
+
         for c in cases:
             for L1, L2 in permutations(LS, 2):
                 i = 0
@@ -23,13 +22,13 @@ class Solution:
                     else:
                         count = -1
                         break
-                
+
                     i += 1
-                
+
                 if count != -1:
                     res.append(count)
-        
+
         if not res:
             return -1
-        
+
         return min(res)
