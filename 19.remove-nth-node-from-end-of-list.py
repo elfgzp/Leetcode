9ccc32eab3f9d92ab69603ef267adbb775985dff
elfgzp <741424975@qq.com>
2019-03-11 15:38:41 +0800

    [ADD] add 19

---
+++ 19.remove-nth-node-from-end-of-list.py
@@ -0,0 +1,60 @@
+#
+# @lc app=leetcode.cn id=19 lang=python3
+#
+# [19] 删除链表的倒数第N个节点
+#
+# https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+#
+# algorithms
+# Medium (32.19%)
+# Total Accepted:    32.2K
+# Total Submissions: 99.1K
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+#
+# 示例：
+#
+# 给定一个链表: 1->2->3->4->5, 和 n = 2.
+#
+# 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+#
+#
+# 说明：
+#
+# 给定的 n 保证是有效的。
+#
+# 进阶：
+#
+# 你能尝试使用一趟扫描实现吗？
+#
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        node = head
+        
+        inn = []
+
+        while node:
+            inn.append(node)
+            node = node.next
+        if n == 1:
+            if len(inn) == 1:
+                return None
+            else:
+                node = inn[-2]
+                node.next = None
+        else:
+            node = inn[-n]
+            node.val, node.next = node.next.val, node.next.next
+
+        return head
+
+
