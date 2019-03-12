62663d4fc36fb11c38a863151a14e4e32303753b
Gzp_ <741424975@qq.com>
2019-03-12 22:22:53 +0800

    [ADD] add 21

---
+++ 21.merge-two-sorted-lists.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+# https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (52.10%)
+# Total Accepted:    48.8K
+# Total Submissions: 92.2K
+# Testcase Example:  '[1,2,4]\n[1,3,4]'
+#
+# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+#
+# 示例：
+#
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+#
+#
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution1:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode(None)
+        node = res
+        while l1 and l2:
+            if l1.val < l2.val:
+                node.next, l1 = l1, l1.next
+            else:
+                node.next, l2 = l2, l2.next
+            node = node.next
+
+        if l1:
+            node.next = l1
+        else:
+            node.next = l2
+
+        return res.next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 or not l2:
+            return l1 or l2
+
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+
+
+
+
