45ae4cf1aafa18eeb98c09671ebbf3732188f862
Gzp_ <741424975@qq.com>
2019-03-14 22:26:35 +0800

    [MOD] Modify 102

---
+++ 102.binary-tree-level-order-traversal.py
@@ -0,0 +1,71 @@
+#
+# @lc app=leetcode.cn id=102 lang=python3
+#
+# [102] 二叉树的层次遍历
+#
+# https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+#
+# algorithms
+# Medium (52.94%)
+# Total Accepted:    18.7K
+# Total Submissions: 34.6K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+#
+# 例如:
+# 给定二叉树: [3,9,20,null,null,15,7],
+#
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+#
+#
+# 返回其层次遍历结果：
+#
+# [
+# ⁠ [3],
+# ⁠ [9,20],
+# ⁠ [15,7]
+# ]
+#
+#
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+
+        inn = []
+        next_inn = [root]
+        res = []
+
+        while inn or next_inn:
+            inn = next_inn
+            next_inn = []
+            level = []
+            while inn:
+
+                node = inn.pop(0)
+                level.append(node.val)
+
+                if node.left:
+                    next_inn.append(node.left)
+
+                if node.right:
+                    next_inn.append(node.right)
+
+            res.append(level)
+
+        return res
+
