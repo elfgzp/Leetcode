13f877afe02e164b43f2f7a61f92a4495ab67454
elfgzp <741424975@qq.com>
2019-04-16 19:59:11 +0800

    [ADD] add 3

---
+++ 3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+# https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+#
+# algorithms
+# Medium (28.17%)
+# Total Accepted:    108.6K
+# Total Submissions: 374.1K
+# Testcase Example:  '"abcabcbb"'
+#
+# 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+#
+# 示例 1:
+#
+# 输入: "abcabcbb"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+#
+#
+# 示例 2:
+#
+# 输入: "bbbbb"
+# 输出: 1
+# 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+#
+#
+# 示例 3:
+#
+# 输入: "pwwkew"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+# 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+#
+#
+#
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res, cur = 0, 0
+        sub = ''
+        for c in s:
+            if c not in sub:
+                cur += 1
+                sub += c
+            else:
+                index = sub.index(c)
+                sub = sub[index + 1:] + c
+                cur = len(sub)
+
+            res = max(res, cur)
+        return res
+
