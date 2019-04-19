274dc70cf767547f0341c87403cf3f9b2c580491
elfgzp <741424975@qq.com>
2019-04-19 14:27:10 +0800

    [MOD] modify 26

--- 26.remove-duplicates-from-sorted-array.py
+++ 26.remove-duplicates-from-sorted-array.py
@@ -53,7 +53,7 @@
 #
 
 
-class Solution:
+class Solution1:
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
@@ -73,3 +73,23 @@ class Solution:
                 i += 1
 
         return len_nums
+
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums) <= 1:
+            return len(nums)
+
+        i, j = 1, 1
+        while i < len(nums):
+            if nums[i] <= nums[j - 1]:
+                i += 1
+            else:
+                nums[j] = nums[i]
+                j += 1
+        
+        return j
