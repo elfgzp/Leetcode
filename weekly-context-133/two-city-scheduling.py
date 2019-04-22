08ba27d12ac7af3d40cbf361778de72d071119c8
elfgzp <741424975@qq.com>
2019-04-22 20:11:35 +0800

    [ADD] update twoCitySchedCost

--- weekly-context-133/two-city-scheduling.py
+++ weekly-context-133/two-city-scheduling.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        # costs.sort(key=lambda x: (-(abs(x[0] - x[1])), x[0] + x[1]))
         costs.sort(key=lambda x: -(abs(x[0] - x[1])))
         
         half = len(costs) // 2
