aa67cca195bfb4e5f87861e801ab4e0fe853ab2c
elfgzp <741424975@qq.com>
2019-04-22 21:02:09 +0800

    Update weekly-context-133/matrix-cells-in-distance-order.py

--- weekly-context-133/matrix-cells-in-distance-order.py
+++ weekly-context-133/matrix-cells-in-distance-order.py
@@ -1,6 +1,7 @@
 from typing import List
 
-class Solution:
+
+class Solution1:
     def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
         res = []
         for r in range(R):
@@ -10,3 +11,10 @@ class Solution:
         res.sort(key=lambda x: x[1])
         res = [x[0] for x in res]
         return res
+
+
+class Solution:
+    def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
+        cells = [(r, c) for c in range(C) for r in range(R)]
+        cells.sort(key=lambda x: abs(x[0] - r0) + abs(x[1] - c0))
+        return cells
