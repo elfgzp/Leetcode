80cdbebda78124383dbe07e76b722c19e2542c24
elfgzp <741424975@qq.com>
2019-04-26 09:54:21 +0800

    [ADD] add weekly-contest-121

---
+++ weekly-contest-121/string-without-aaa-or-bbb.py
@@ -0,0 +1,58 @@
+class Solution:
+    def strWithout3a3b(self, A, B):
+        """
+        :type A: int
+        :type B: int
+        :rtype: str
+        """
+        a = ['a'] * A
+        b = ['b'] * B
+        res = []
+        while a or b:
+
+            if A > B:
+                if a:
+                    res.append(a.pop(0))
+                    A -= 1
+                    if a:
+                        res.append(a.pop(0))
+                        A -= 1
+                if b:
+                    res.append(b.pop(0))
+                    B -= 1
+            elif A < B:
+                if b:
+                    res.append(b.pop(0))
+                    B -= 1
+                    if b:
+                        res.append(b.pop(0))
+                        B -= 1
+                if a:
+                    res.append(a.pop(0))
+                    A -= 1
+
+            if abs(A - B) <= 1:
+                if res:
+                    if res[-1] == 'a':
+                        if b:
+                            res.append(b.pop(0))
+                            B -= 1
+                        if a:
+                            res.append(a.pop(0))
+                            A -= 1
+                    else:
+                        if a:
+                            res.append(a.pop(0))
+                            A -= 1
+                        if b:
+                            res.append(b.pop(0))
+                            B -= 1
+                else:
+                    if a:
+                        res.append(a.pop(0))
+                        A -= 1
+                    if b:
+                        res.append(b.pop(0))
+                        B -= 1
+
+        return ''.join(res)
