39447342f271d138d3942c09bc500db0b1284a81
elfgzp <741424975@qq.com>
2019-05-08 10:10:59 +0800

    [ADD] add 338

--- 338.counting-bits.py
+++ 338.counting-bits.py
@@ -3,7 +3,25 @@
 #
 # [338] 比特位计数
 #
+
+
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        
+        dp = [0, 1]
+        if num < 2:
+            return dp[:num + 1]
+
+        k = 1
+
+        for i in range(2, num + 1):
+            if i >= k * 2:
+                k *= 2
+
+            if i == k:
+                dp.append(1)
+            else:
+                dp.append(dp[k] + dp[i - k])
+
+        return dp
+
 
