60f99558f68884349a6d153f49f6d7500859ad3c
Gzp_ <741424975@qq.com>
2019-05-14 22:11:25 +0800

    [ADD] add 62

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -42,11 +42,24 @@
 #
 
 
-class Solution:
+class Solution(object):
     def uniquePaths(self, m, n):
         """
         :type m: int
         :type n: int
         :rtype: int
         """
+        if m == 1 or n == 1:
+            return 1
+        
+        dp = [[0] * m for _ in range(n)]
+        dp[0][:] = [1] * m
+        for i in range(n):
+            dp[i][0] = 1
+            
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        return dp[-1][-1]
 
