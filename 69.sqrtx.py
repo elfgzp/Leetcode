84e8b9214fa56e0560e3272ffc26eaba7bc13aa9
Gzp_ <741424975@qq.com>
2019-06-01 12:12:36 +0800

    [ADD] 69

--- 69.sqrtx.py
+++ 69.sqrtx.py
@@ -1,11 +1,32 @@
-class Solution:
+class Solution1:
+    """
+    二分查找法
+    """
     def mySqrt(self, x: int) -> int:
         if x <= 1:
             return x
         
-        n = x
-        xn = n // 2
-        while xn**2 - n >= 1:
-            xn = xn - (xn**2 - n) / (2 * xn)
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) / 2.0
+            if int(mid**2) > x:
+                right = mid
+            elif int(mid**2) < x:
+                left = mid
+            else:
+                return int(mid)
+            
+    
+class Solution:
+    """
+    牛顿迭代法
+    """
+    def mySqrt(self, x: int) -> int:
+        if x <= 1:
+            return x
+        
+        ans = x / 2.0
+        while ans**2 - x >= 1:
+            ans = (ans + x / ans) / 2
 
-        return int(xn)
+        return int(ans)
