0461ff3264fcedfc3d2a26ccc784cbf0dedef2d8
Gzp_ <741424975@qq.com>
2019-07-03 20:58:48 +0800

    [MOD] 779

--- 779.k-th-symbol-in-grammar.py
+++ 779.k-th-symbol-in-grammar.py
@@ -2,13 +2,13 @@ class Solution:
     def kthGrammar(self, N: int, K: int) -> int:
         if N == 1:
             return 0
+        
         if N == 2:
             return 0 if K == 1 else 1
         
-        prev_len = 2**(N - 2)
-        if K <= prev_len:
+        half = 2**(N - 2)
+        if K <= half:
             return self.kthGrammar(N - 1, K)
         else:
-            return 1 - self.kthGrammar(N - 1, K - prev_len)
-
+            return 1 - self.kthGrammar(N - 1, K - half)
 
