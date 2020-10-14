1ee00678afa1a3f85535b4d66af9692af9186b02
elfgzp <me@elfgzp.cn>
2020-10-14 17:40:51 +0800

    feat: merge sort

---
+++ sort/mergesort/mergesort.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print(MergeSort([]int{6, 5, 4, 3, 2, 1}))
+}
+
+func MergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := MergeSort(arr[:mid])
+	right := MergeSort(arr[mid:])
+
+	l, r := 0, 0
+	result := make([]int, 0)
+
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			result = append(result, left[l])
+			l++
+		} else {
+			result = append(result, right[r])
+			r++
+		}
+	}
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+	return result
+}
